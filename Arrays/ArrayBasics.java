f45a2edd476349400db27dd2ec4d4ae6e42cb2e0
Saransh-14 <saransh.sharma.7860@gmail.com>
2026-05-10 18:08:10 +0530

    Day:1 Array basics and git setup

---
+++ Arrays/ArrayBasics.java
@@ -0,0 +1,6 @@
+public class ArrayBasics{
+    public static void main(String[] args){
+        int[] arr = {7,8,4,5,85,96,14};
+        System.out.println("The first element is:"+arr[0]);
+    }
+}
