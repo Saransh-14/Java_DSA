1cc0019da7ddcf7aae31936ab7954ae89c95f506
Saransh-14 <saransh.sharma.7860@gmail.com>
2026-09-03 22:17:06 +0530

    Performed some basic operations on trees

--- Tree/treeOperations.java
+++ Tree/treeOperations.java
@@ -10,15 +10,20 @@ public class treeOperations{
             return 1 + countNode(n.left) + countNode(n.right);
         }
 
+        public static int sumNode(Tree.Node n){
+            if (n==null) return 0;
+            return n.data + sumNode(n.left) + sumNode(n.right);
+        }
+
 
         public static void main(String[] args){
-        Tree.Node root = new Tree.Node(1);
-        root.left = new Tree.Node(2);
-        root.right = new Tree.Node(3);
+        Tree.Node root = new Tree.Node(5);
+        root.left = new Tree.Node(3);
+        root.right = new Tree.Node(8);
         // root.right.right = new Tree.Node(5);
         // root.right.right.right = new Tree.Node(6);
-        root.left.right = new Tree.Node(5);
-        root.left.left = new Tree.Node(4);
+        root.left.right = new Tree.Node(4);
+        root.left.left = new Tree.Node(2);
 
         System.out.println(countNode(root));
     }
